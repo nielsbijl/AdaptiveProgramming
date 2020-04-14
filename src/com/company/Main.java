999ad025e969ee4741c761c8f8d3074186f605c4
Niels <nbijl@petersplanet.nl>
2020-04-14 10:13:09 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int x = 1;
+        while (x <= 4)
+        {
+            System.out.println("Value of x:" + x);
+            x++;
+        }
+
+    }}
